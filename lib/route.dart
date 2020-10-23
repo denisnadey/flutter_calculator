3665004748e1bef38e4bcd252fb1485cd8e41fa8
unknown <denis.nadey@gmail.com>
2020-10-23 21:36:32 +0300

    fonts

--- lib/route.dart
+++ lib/route.dart
@@ -1,5 +1,6 @@
 import 'package:cloudtech_calculator/theme_model.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class SecondRoute extends StatefulWidget {
@@ -43,7 +44,7 @@ class _SecondRouteState extends State<SecondRoute> {
                           .toggleTheme();
                     },
                   ),
-                )
+                ),
               ],
             ),
           )),
@@ -61,8 +62,16 @@ class _CursRouteState extends State<CursState> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Курсы валют в рублях",
-            style: TextStyle(color: Theme.of(context).accentColor)),
+        title: Text(
+          "NOTHING",
+          style: GoogleFonts.rubik(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).accentColor,
+            letterSpacing: 3.06,
+          ),
+          textAlign: TextAlign.center,
+        ),
         shadowColor: const Color(0x00000000),
       ),
       body: Container(
@@ -72,25 +81,61 @@ class _CursRouteState extends State<CursState> {
             child: ListView(
               padding: const EdgeInsets.all(8),
               children: [
-                ListTile(
-                  leading: IconButton(
-                      icon: Icon(
-                        Icons.settings_display,
-                        color: Theme.of(context).accentColor,
-                      ),
-                      onPressed: null),
-                  title: Text(
-                    "Светлая тема",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: Theme.of(context).accentColor),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Theme.of(context).backgroundColor,
                   ),
-                  trailing: Switch(
-                    value: Theme.of(context).brightness == Brightness.light,
-                    onChanged: (value) {
-                      Provider.of<ThemeModel>(context, listen: false)
-                          .toggleTheme();
-                    },
+                  child: ListTile(
+                    leading: IconButton(
+                        icon: Icon(
+                          Icons.settings_display,
+                          color: Theme.of(context).accentColor,
+                        ),
+                        onPressed: null),
+                    title: Text(
+                      "USD",
+                      style: GoogleFonts.rubik(
+                          fontSize: 21,
+                          fontWeight: FontWeight.w400,
+                          color: Theme.of(context).accentColor),
+                    ),
+                    trailing: Text(
+                      "77,96 ₽",
+                      style: GoogleFonts.rubik(
+                          fontSize: 21,
+                          fontWeight: FontWeight.w500,
+                          color: Theme.of(context).accentColor),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Theme.of(context).backgroundColor,
+                  ),
+                  child: ListTile(
+                    leading: IconButton(
+                        icon: Icon(
+                          Icons.settings_display,
+                          color: Theme.of(context).accentColor,
+                        ),
+                        onPressed: null),
+                    title: Text(
+                      "USD",
+                      style: GoogleFonts.rubik(
+                          fontSize: 21,
+                          fontWeight: FontWeight.w400,
+                          color: Theme.of(context).accentColor),
+                    ),
+                    trailing: Text(
+                      "77,96 ₽",
+                      style: GoogleFonts.rubik(
+                          fontSize: 21,
+                          fontWeight: FontWeight.w500,
+                          color: Theme.of(context).accentColor),
+                    ),
                   ),
                 )
               ],
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
-  primaryColor: const Color(0xff131618),
-  accentColor: Colors.white,
+  primaryColor: const Color(0xff20274C),
+  accentColor: const Color(0xffD4D9EC),
+  backgroundColor: const Color(0xff414d7b),
   brightness: Brightness.dark,
   textTheme: const TextTheme(bodyText2: TextStyle(color: Color(0xffffffff))),
 );
@@ -10,6 +11,7 @@ ThemeData darkTheme = ThemeData(
 ThemeData lightTheme = ThemeData(
   primaryColor: Colors.white,
   accentColor: Colors.green,
+  backgroundColor: const Color(0xff414d7b),
   brightness: Brightness.light,
   textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.green)),
 );
